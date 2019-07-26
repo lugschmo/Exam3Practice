0283b16ae9ba464c229edc9bbc3079f600c485c7
lugschmo <lugschmo@rose-hulman.edu>
2019-07-26 17:54:39 -0600

    done 3

--- src/m4_sequences.py
+++ src/m4_sequences.py
@@ -5,8 +5,8 @@ This problem provides practice at:
   ***  SEQUENCES.  ***
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Mitch Lugsch.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 ########################################################################
 # Students:
@@ -134,13 +134,18 @@ def practice_problem4a(sequence):
       :type sequence: list | tuple | string
     """
     ####################################################################
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #     The testing code is already written for you (above).
     ####################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
     #    DIFFICULTY:      7
     #    TIME ESTIMATE:   15 minutes.
     ####################################################################
+    seq = []
+    for k in range(len(sequence) - 1):
+        if sequence[k + 1] == sequence[k]:
+            seq = seq + [k]
+    return seq
 
 
 def run_test_practice_problem4b():
@@ -197,13 +202,21 @@ def practice_problem4b(sequence):
       :type sequence: (list | tuple) of (float | int)
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #     The testing code is already written for you (above).
     ####################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
     #    DIFFICULTY:      5
     #    TIME ESTIMATE:   10 minutes.
     ####################################################################
+    k_for_max = 0
+    for k in range(len(sequence)):
+        if k % 2 == 1:
+            pass
+        else:
+            if sequence[k] > sequence[k_for_max]:
+                k_for_max = k
+    return sequence[k_for_max]
 
 
 def run_test_practice_problem4c():
